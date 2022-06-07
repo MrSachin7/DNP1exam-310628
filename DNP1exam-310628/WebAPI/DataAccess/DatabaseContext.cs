86430115af11136f12feb09a8e65fa1ab9c07d6f
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-06-07 08:48:01 +0200

    Initial commit

---
+++ DNP1exam-310628/WebAPI/DataAccess/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.DataAccess; 
+
+public class DatabaseContext :DbContext {
+
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+        // please use your own root path...
+        optionsBuilder.UseSqlite(
+            @"Data Source =C:\Users\nepal\OneDrive\Desktop\DNP-Exam-310628\DNP1exam-310628\WebAPI\database.db");
+    }
+}
